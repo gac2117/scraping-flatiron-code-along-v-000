16b4e588d007a552de6d9d66964e07579529677c
Grace Choi <oiograceoio@gmail.com>
2019-01-11 09:20:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -18,11 +19,11 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses/"))
-    binding.pry 
+    binding.pry
   end
 
-  def get_courses 
-    
+  def get_courses
+
   end
 end
 
